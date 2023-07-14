a226ca31a5146ee671fa3adbbdd411cf57f5d578
DimitarZhelev92 <138435392+DimitarZhelev92@users.noreply.github.com>
2023-07-14 11:18:31 +0300

    Create praktika_petyk.py

---
+++ praktika_petyk.py
@@ -0,0 +1,36 @@
+def numbers_to_msg(nums):
+    chars = {0:'',
+            1:'abc',
+            2:'def',
+            3:'ghi',
+            4:'jkl',
+            5:'mno',
+            6:'pqrs',
+            7:'tuv',
+            8:'wxyz'}
+    group = []
+    temp_group = []
+
+    for el in nums:
+        if temp_group == []:
+            temp_group.append(el)
+        
+        if el in temp_group:
+            temp_group.append(el)
+        
+        else:
+            if el > 0:
+                group.append(temp_group)
+                temp_group = []
+                temp_group.append(el)
+            else:
+                group.append(temp_group)
+                temp_group = []
+    group.append(temp_group)
+    result = ''
+    for sub_group in group:
+        char_pos = len(sub_group) % len(chars[sub_group[0]]) -1
+        result = result + chars[sub_group[0]][char_pos]
+    return result
+
+print(numbers_to_msg([2,-1,2,2,-1,2,2,2]))
